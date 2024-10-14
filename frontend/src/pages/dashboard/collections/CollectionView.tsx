593fd9a71d61d5b6fddf88ebbaff202a914e8727
Superfelicia <felicia.ahlstrom@hotmail.se>
2024-10-14 10:53:47 +0200

    Added collectionView to view collections status and more

--- frontend/src/pages/dashboard/collections/CollectionView.tsx
+++ frontend/src/pages/dashboard/collections/CollectionView.tsx
@@ -1,16 +1,18 @@
 import { Button } from '@/components/ui/button'
+import { Collection } from '@/interfaces/Collection'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const CollectionView = () => {
     const { collectionId } = useParams()
-    const [collection, setCollection] = useState(null)
+    const [collection, setCollection] = useState<Collection | null>(null)
 
     useEffect(() => {
         const fetchCollection = async () => {
             const response = await fetch(`http://localhost:3000/api/collections/${collectionId}`)
-            const data = await response.json()
+            const data: Collection = await response.json()
             setCollection(data)
+            console.log(collection)
         }
 
         fetchCollection()
@@ -20,7 +22,7 @@ const CollectionView = () => {
 
     return (
         <div>
-            <h1>{collection.title}</h1>
+            <h1>{collection.name}</h1>
             <p>Status: {collection.status}</p>
             <p>Progress: {collection.progress}%</p>
             <p>Deadline: {new Date(collection.deadline).toLocaleDateString()}</p>
