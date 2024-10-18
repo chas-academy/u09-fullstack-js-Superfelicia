c2c056f00914358bb4032cb63f6fae9e7f8ec09f
Superfelicia <felicia.ahlstrom@hotmail.se>
2024-10-18 15:29:22 +0200

    allowedheaders added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: "https://flashlearn09.netlify.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
